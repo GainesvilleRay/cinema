a7a621cd7581680aa373c81233aaf642f3877b20
Doug Ray <doug.ray@gvillesun.com>
2018-11-30 14:02:53 -0500

    work

---
+++ cinamaformat.py
@@ -0,0 +1,38 @@
+holygrail : {
+    title : holygrail,
+    rating : pg,
+    cinema : regal,
+    times : 1, 3, 5
+    }
+
+holygrail : {
+    title : holygrail,
+    rating : pg,
+    cinema : amc,
+    times : 2, 4, 6
+    }
+
+lifeofbrian: {
+    title : lifeofbrian,
+    rating : r,
+    cinema : regal,
+    times : 1, 3, 5
+    }
+
+lifeofbrian : {
+    title : casablanca,
+    rating : r,
+    cinema : amc,
+    times : 2, 4, 6
+    }
+
+holygrail : {
+    title: holygrail
+    rating: pg
+    cinema1 : {cinema1 : regal,
+        
+
+
+
+    }
+}
