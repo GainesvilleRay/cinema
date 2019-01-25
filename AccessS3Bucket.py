852779526da83a5433f2c85d60b53c0cd47e1782
GainesvilleRay <doug.ray@starbanner.com>
2019-01-25 19:50:14 +0000

    dev and test

--- AccessS3Bucket.py
+++ AccessS3Bucket.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python3.6
+
 import boto3
 import botocore
 from datetime import tzinfo, timedelta, datetime
 import datetime
+from email.message import EmailMessage
+import smtplib
+
+# Who gets the email
+receiver = ['doug.ray@starbanner.com']
+
+# Which market is reported
+market = 'Gainesville and Ocala, for now'
 
 # Download most recent cinema scrape from AWS S3
 s3c = boto3.client('s3')
@@ -31,3 +41,25 @@ else:
     msg = "No new cinemascrape file today"
 
 print(msg)
+
+# SEND REPORT to recipient(s).
+#with open(bigreport) as fp:
+# Create a text/plain message
+msg = EmailMessage()
+msg.set_content("No content yet")
+
+sender = 'data@sunwriters.com'
+gmail_password = '%WatchingTheDetectives'
+msg['Subject'] = f'Latest cinema scrape for {market}'
+msg['from'] = sender
+msg['To'] = receiver
+
+# Send the message via our own SMTP server.
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(sender, gmail_password)
+    server.send_message(msg)
+    server.quit()
+except:
+    print('Something went wrong...')
