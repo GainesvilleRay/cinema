420acebfc8a26a29d975dae30c84d515935c0ebb
Doug Ray <doug.ray@gvillesun.com>
2019-05-20 17:51:05 -0400

    new work

---
+++ creds.py
@@ -0,0 +1,7 @@
+"""
+Variables for use in scripts to access data@sunwriters.com
+"""
+
+receiver = "doug.ray@starbanner.com"
+gmail_address = 'data@sunwriters.com'
+gmail_password = '%WatchingTheDetectives'
