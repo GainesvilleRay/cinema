fa250c6b831c3a9179b0c235911af8200402ada1
Douglas Ray <doug.ray@gvillesun.com>
2019-05-21 12:12:00 -0400

    Delete creds.py

--- creds.py
+++
@@ -1,7 +0,0 @@
-"""
-Variables for use in scripts to access data@sunwriters.com
-"""
-
-receiver = "doug.ray@starbanner.com"
-gmail_address = 'data@sunwriters.com'
-gmail_password = '%WatchingTheDetectives'
